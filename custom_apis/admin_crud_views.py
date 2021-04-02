3e79c004338f225f4b5a9c4c8b8efe00d9c2ac9c
Aditya Mali <41832893+maliaditya@users.noreply.github.com>
2021-04-02 16:10:15 +0530

    is blocked query updated

--- custom_apis/admin_crud_views.py
+++ custom_apis/admin_crud_views.py
@@ -7,7 +7,7 @@ from rest_framework.decorators import api_view,permission_classes
 from rest_framework.permissions import IsAuthenticated
 import requests
 import json
-
+from authapp.models import User
 
 
 
@@ -15,8 +15,7 @@ import json
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def block(request,st,user_id):
-    cursor=connection.cursor()
-    cursor.execute(f' update authapp_user set isblocked = {st} where id = {user_id}')
+    User.objects.filter(id=user_id).update(isblocked = st)
     return Response(data = {"message": "user blocked success"},status = status.HTTP_200_OK)
 
 @api_view(['GET'])
